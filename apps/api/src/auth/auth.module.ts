66bed034cf75474ef8d05bb7f9dbf93046887ff8
stringnet <135693628+stringnet@users.noreply.github.com>
2025-06-09 22:20:34 -0500

    Update auth.module.ts

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+// apps/api/src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -5,7 +6,7 @@ import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [JwtModule.register({})],
-  controllers: [AuthController],
+  controllers: [AuthController], // <-- ¿Está AuthController aquí en la lista?
   providers: [AuthService],
 })
 export class AuthModule {}
