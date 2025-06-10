494c113adf4b33bf42d235a3f1115988b027446b
stringnet <135693628+stringnet@users.noreply.github.com>
2025-06-10 05:05:46 -0500

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,4 @@
+// apps/api/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,19 +6,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitamos CORS directamente en NestJS.
-  // Esta configuración ahora será la que reciba la petición del navegador.
   app.enableCors({
     origin: 'https://adminwebra.scanmee.io',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-  }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  // NestJS escuchará en el puerto 3000 dentro del contenedor
   await app.listen(3000);
 }
 bootstrap();
