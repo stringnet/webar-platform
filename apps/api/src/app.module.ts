767fe4f4e8921177c6ed45f8d78f50f2291a8848
stringnet <135693628+stringnet@users.noreply.github.com>
2025-06-09 21:23:39 -0500

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,13 @@
-// apps/api/src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module'; // Esta ruta ahora es correcta
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config'; // <-- Importar
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({ // <-- Añadir esto
+      isGlobal: true, // <-- Hace que las variables estén disponibles en todos los módulos
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
