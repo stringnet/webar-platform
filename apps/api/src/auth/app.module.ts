cd2c3fd95b43a249a14d34ecf133469c33780f16
stringnet <135693628+stringnet@users.noreply.github.com>
2025-06-09 20:52:07 -0500

    Delete apps/api/src/auth/app.module.ts

--- apps/api/src/auth/app.module.ts
+++
@@ -1,8 +0,0 @@
-// apps/api/src/app.module.ts
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-
-@Module({
-  imports: [AuthModule], // <-- AÑADIR ESTO
-})
-export class AppModule {}
