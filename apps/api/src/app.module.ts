b6785997fcbfbeabfe0e41bd16ca110beb3c5e7b
stringnet <135693628+stringnet@users.noreply.github.com>
2025-06-09 20:48:15 -0500

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,8 @@
+// apps/api/src/app.module.ts
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module'; // Esta ruta ahora es correcta
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [AuthModule],
 })
 export class AppModule {}
