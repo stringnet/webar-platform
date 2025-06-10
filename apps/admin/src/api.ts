bcaf094e5e8aa5e5241a2c53412bf186e71fb457
stringnet <135693628+stringnet@users.noreply.github.com>
2025-06-09 20:43:23 -0500

    Update api.ts

--- apps/admin/src/api.ts
+++ apps/admin/src/api.ts
@@ -1,7 +1,25 @@
+// apps/admin/src/api.ts
 import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.webra.scanmee.io', // La URL de tu API desplegada
 });
+
+// ¡Esto es un interceptor! Se ejecuta ANTES de cada petición.
+api.interceptors.request.use(
+  (config) => {
+    // 1. Obtiene el token del localStorage
+    const token = localStorage.getItem('accessToken');
+
+    // 2. Si el token existe, lo añade a las cabeceras (headers)
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
