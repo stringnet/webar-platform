775c03a15fb05c14751101c797d2a5b7f86f93ac
stringnet <roberto@stringnet.pe>
2025-06-11 01:53:15 -0500

    feat(api): setup local db and fix final build process

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
-set -e 
+set -e # Salir inmediatamente si un comando falla
 
+# Usamos npm run para la migración
 echo "Running database migrations..."
-npx prisma migrate deploy
+npm run prisma:deploy
 echo "Database migrations completed."
 
+# Usamos npm run para la generación del cliente
 echo "Generating Prisma client..."
-npx prisma generate
+npm run prisma:generate
 echo "Prisma Client generated successfully."
 
+# Inicia la aplicación NestJS en segundo plano
 echo "Starting NestJS app..."
 node dist/main &
 
+# Inicia Nginx en primer plano
 echo "Starting Nginx..."
 nginx -g 'daemon off;'
