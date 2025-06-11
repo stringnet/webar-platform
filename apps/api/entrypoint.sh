12d16f5b20023e6f583f5e9929de8a5e0b4e35e8
stringnet <roberto@stringnet.pe>
2025-06-11 02:44:56 -0500

    fix(api):Correcion y actualizacion a dockerfile y entrypoint.sh

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 set -e # Salir inmediatamente si un comando falla
 
-# Usamos npm run para la migración
+# ¡Paso de Auto-Reparación!
+# Damos permisos a todas las herramientas dentro de node_modules/.bin
+echo "Setting execute permissions..."
+chmod +x /usr/src/app/node_modules/.bin/*
+echo "Permissions set."
+
+# 1. Ejecutar la migración de la base de datos al arrancar.
 echo "Running database migrations..."
 npm run prisma:deploy
 echo "Database migrations completed."
 
-# Usamos npm run para la generación del cliente
+# 2. Generar el cliente de Prisma
 echo "Generating Prisma client..."
 npm run prisma:generate
 echo "Prisma Client generated successfully."
 
-# Inicia la aplicación NestJS en segundo plano
+# 3. Inicia la aplicación NestJS en segundo plano
 echo "Starting NestJS app..."
 node dist/main &
 
-# Inicia Nginx en primer plano
+# 4. Inicia Nginx en primer plano
 echo "Starting Nginx..."
 nginx -g 'daemon off;'
