0b7c9147cd299d3899108f025277d477674fbc57
stringnet <roberto@stringnet.pe>
2025-06-11 23:21:01 -0500

    fix(get-user.decorator.ts) update

--- apps/api/src/auth/decorator/get-user.decorator.ts
+++ apps/api/src/auth/decorator/get-user.decorator.ts
@@ -1,9 +1,13 @@
-// apps/api/src/auth/decorator/get-user.decorator.ts
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 export const GetUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (data: string | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.user;
+    const user = request.user as User;
+
+    // Si pasamos un dato como @GetUser('id'), devolvemos solo esa propiedad.
+    // Si no, devolvemos el objeto de usuario completo.
+    return data ? user?.[data] : user;
   },
 );
