011e6b2599620d654a4e084bddd009d674a6f00f
stringnet <roberto@stringnet.pe>
2025-06-17 18:03:35 -0500

    fix(module.ts):delete storage

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProjectsModule } from './projects/projects.module';
-import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module'; // Importamos Cloudinary
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     ProjectsModule,
-    CloudinaryModule, // <-- Asegúrate de que CloudinaryModule esté aquí
+    CloudinaryModule, // <-- El único módulo de almacenamiento que debe estar aquí
   ],
 })
 export class AppModule {}
