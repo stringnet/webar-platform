ba4a43fe347cd91e2fb3f7ebfa5e3ef419fdcb3f
stringnet <roberto@stringnet.pe>
2025-06-18 00:13:52 -0500

    fix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,3 @@
-// apps/api/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -9,13 +8,9 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const clientUrl = configService.get<string>('CORS_ORIGIN');
 
-  // --- LÍNEA CLAVE AÑADIDA / VERIFICADA ---
-  // Habilitamos CORS para que el admin pueda hablar con la API.
   app.enableCors({
-    origin: clientUrl, // Lee el dominio del admin desde las variables de entorno
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: clientUrl,
   });
-  // ------------------------------------
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
