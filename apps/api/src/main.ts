24fab87d55d682472590de5e21b926cbc6542ddb
stringnet <roberto@stringnet.pe>
2025-06-17 20:28:17 -0500

    fix(dockefile - main.ts)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://adminwebra.scanmee.io' });
+  const configService = app.get(ConfigService);
+  const clientUrl = configService.get<string>('CORS_ORIGIN');
+
+  app.enableCors({
+    origin: clientUrl,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
