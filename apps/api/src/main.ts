24b0c53b98bf03aea4d0e867149cc367d88a3703
stringnet <roberto@stringnet.pe>
2025-06-17 19:26:20 -0500

    fix(api); dockerfile - main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const clientUrl = configService.get<string>('CORS_ORIGIN');
 
   app.enableCors({
-    origin: 'https://adminwebra.scanmee.io',
+    origin: clientUrl,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
